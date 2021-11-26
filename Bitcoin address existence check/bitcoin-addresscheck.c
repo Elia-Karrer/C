c3ebfec6f8e46dc3332f8af6bbf76afc04b142b1
Elia Karrer <92625537+Elia-Karrer@users.noreply.github.com>
2021-11-26 22:54:12 +0100

    Update bitcoin-addresscheck.c

--- Bitcoin address existence check/bitcoin-addresscheck.c
+++ Bitcoin address existence check/bitcoin-addresscheck.c
@@ -2,6 +2,9 @@
     Bitcoin / Bitcoin Cash / Dogecoin - Address existence checker
     All rights reserved
     Elia Karrer, 2021
+
+    My BTC address:
+    1ELiK2BCxEfZNRHAr7rkXwQ9AY7Lz1MEJv
 **/
 
 #include <stdio.h>
@@ -16,22 +19,23 @@
 #define MJ(a, b, c) ((a&b) ^ (a&c) ^ (b&c))
 #define e32 0xFFFFFFFF
 
-typedef unsigned int WRD;
-const WRD sqrts[8] = {0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19};
-const WRD cons[64] = {0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5, 0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174, 0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da, 0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967, 0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85, 0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070, 0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3, 0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2};
-unsigned char HEX[] = "0123456789abcdef";
-unsigned char HEX2[] = "0123456789ABCDEF";
-unsigned char BASE58[] = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+const unsigned int sqrts[8] = {0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19};
+const unsigned int cons[64] = {0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5, 0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174, 0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da, 0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967, 0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85, 0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070, 0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3, 0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2};
+const unsigned char * BASE58 = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
 
+typedef unsigned int WRD;
 typedef struct Block BLOCK;
+typedef struct uint256_t uint256;
+
 struct Block
 {
     WRD b[64];
     WRD content;
 };
-
-typedef struct uint256_t uint256;
-struct uint256_t {unsigned int word[8];};
+struct uint256_t
+{
+    WRD word[8];
+};
 
 
 void sha256(unsigned char *s, WRD *ret, int len)
@@ -103,49 +107,6 @@ void sha256(unsigned char *s, WRD *ret, int len)
         ret[i] = out[i];
 }
 
-void printhash(WRD *a)
-{
-    for(int i = 0; i < 8; i++)
-        printf("%08x", a[i]);
-    printf("\n");
-}
-
-int get_hex(unsigned char a)
-{
-    for(int i = 0; i < 16; i++)
-        if(a == HEX[i])
-            return i;
-    for(int i = 0; i < 16; i++)
-        if(a == HEX2[i])
-            return i;
-}
-
-int get_base58(unsigned char c)
-{
-    for(int i = 0; i < 58; i++)
-        if(c == BASE58[i])
-            return i;
-}
-
-void WRDtostr(WRD *a, unsigned char *c)
-{
-    WRD t;
-    int i, j;
-
-    for(i = 0; i < 64; i++)
-        c[i] = "0";
-
-    for(i = 0; i < 8; i++)
-    {
-        t = a[i];
-        for(j = 0; j < 8; j++)
-        {
-            c[i*8+7-j] = HEX[t&15];
-            t >>= 4;
-        }
-    }
-}
-
 void WRDtochar(WRD *a, unsigned char *c)
 {
     WRD t;
@@ -164,6 +125,13 @@ void WRDtochar(WRD *a, unsigned char *c)
         }
     }
     c[32] = NULL;
+}
+
+int get_base58(unsigned char c)
+{
+    for(int i = 0; i < 58; i++)
+        if(c == BASE58[i])
+            return i;
 }
 
 void address_base58_to_bin(unsigned char *address, bool *array)
@@ -311,7 +279,7 @@ int main()
     gets(input);
 
     if(check_address(&input))
-        printf("--> address exists\n");
+        printf("--> Address exists\n");
     else
-        printf("--> address does not exist\n");
+        printf("--> Address does not exist\n");
 }
